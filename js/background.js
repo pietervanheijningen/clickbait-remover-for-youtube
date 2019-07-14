5c5c348ed203f1720cd66a22838ba7c731ad47bc
pietervanheijningen <pietervanheijningen@outlook.com>
2019-07-14 14:05:49 +0200

    Add automatic loading of functionality to existing tabs when extenion is enabled

--- js/background.js
+++ js/background.js
@@ -1,3 +1,14 @@
+chrome.tabs.query({url: '*://www.youtube.com/*'}, function (tabs) {
+    tabs.forEach(function (tab) {
+        chrome.tabs.insertCSS(tab.id, {
+            file: 'css/youtube.css'
+        });
+        chrome.tabs.executeScript(tab.id, {
+            file: 'js/replaceThumbnails.js'
+        })
+    })
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
         return {redirectUrl: details.url.replace('hqdefault.jpg', 'hq2.jpg')};
@@ -8,5 +19,3 @@ chrome.webRequest.onBeforeRequest.addListener(
     },
     ['blocking']
 );
-
-console.log('started');
---
+++ js/replaceThumbnails.js
@@ -0,0 +1,7 @@
+let imgElements = document.getElementsByTagName('img');
+
+for (let i = 0; i < imgElements.length; i++) {
+    if (imgElements[i].src.match('https://i.ytimg.com/vi/.*/hqdefault.jpg?.*')) {
+        imgElements[i].src = imgElements[i].src.replace('hqdefault.jpg', 'hq2.jpg')
+    }
+}
