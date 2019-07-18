9dbb5d0e0cba69dc3ece1d012df83bac17c71828
pietervanheijningen <pietervanheijningen@outlook.com>
2019-07-18 19:27:32 +0200

    Add fallback to return to the normal hqdefault if it 404's

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+let noRedirectToken = 'zctf420otaqimwn9lx8m';
+
 chrome.tabs.query({url: '*://www.youtube.com/*'}, function (tabs) {
     tabs.forEach(function (tab) {
         chrome.tabs.insertCSS(tab.id, {
@@ -11,10 +13,25 @@ chrome.tabs.query({url: '*://www.youtube.com/*'}, function (tabs) {
 
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        return {redirectUrl: details.url.replace('hqdefault.jpg', 'hq1.jpg')};
+        if (!details.url.includes(`&noRedirectToken=${noRedirectToken}`)) {
+            return {redirectUrl: details.url.replace('hqdefault.jpg', 'hq1.jpg')};
+        }
     },
     {
         urls: ['https://i.ytimg.com/vi/*/hqdefault.jpg?*'],
+        types: ['image']
+    },
+    ['blocking']
+);
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function (details) {
+        if (details.statusCode === 404) {
+            return {redirectUrl: details.url.replace('hq1.jpg', 'hqdefault.jpg') + `&noRedirectToken=${noRedirectToken}`};
+        }
+    },
+    {
+        urls: ['https://i.ytimg.com/vi/*/hq1.jpg?*'],
         types: ['image']
     },
     ['blocking']
