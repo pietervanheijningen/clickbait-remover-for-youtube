f0eceb5f21582b7e0c046c322926d7e459a7bb1c
pietervanheijningen <pietervanheijningen@outlook.com>
2019-08-29 23:10:18 +0200

    Add page selector to options menu

--- js/options_popup.js
+++ js/options_popup.js
@@ -15,6 +15,23 @@ chrome.storage.sync.get(optionKeys, function (storage) {
     })
 });
 
+chrome.storage.sync.get('enabled_pages', function (storage) {
+    for (let tile of document.getElementsByClassName('pageSelectorTile')) {
+        if (storage.enabled_pages[tile.id]) {
+            tile.classList.add('activeTile');
+            document.getElementById(`checkbox_${tile.id}`).checked = true;
+        }
+
+        tile.addEventListener('click', function () {
+            storage.enabled_pages[tile.id] = !tile.classList.contains('activeTile');
+            tile.classList.toggle('activeTile');
+            document.getElementById(`checkbox_${tile.id}`).checked = storage.enabled_pages[tile.id];
+
+            chrome.storage.sync.set({'enabled_pages': storage.enabled_pages});
+        })
+    }
+});
+
 document.getElementById('pageSelectorTab').onclick = function () {
     this.classList.add('activeTab');
     document.getElementById('videoSettingsTab').classList.remove('activeTab');
