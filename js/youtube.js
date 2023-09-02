08c76168b30ab6f29a4b96a46074d9325fb4ab04
pietervanheijningen <pietervanheijningen@outlook.com>
2023-09-02 21:23:53 +0200

    Apply youtubes new A/B thumbnails to all thumbnail types

--- js/youtube.js
+++ js/youtube.js
@@ -104,9 +104,9 @@ if (typeof window.styleElement === 'undefined') { // shitty way to detect if scr
         let imgElements = document.getElementsByTagName('img');
 
         for (let i = 0; i < imgElements.length; i++) {
-            if (imgElements[i].src.match('https://i.ytimg.com/vi/.*/(hq1|hq2|hq3|hqdefault|mqdefault|hq720|hq720_custom_[0-9]+).jpg?.*')) {
+            if (imgElements[i].src.match('https://i.ytimg.com/(vi|vi_webp)/.*/(hq1|hq2|hq3|hqdefault|mqdefault|hq720)(_custom_[0-9]+)?.jpg?.*')) {
 
-                let url = imgElements[i].src.replace(/(hq1|hq2|hq3|hqdefault|mqdefault|hq720|hq720_custom_[0-9]+).jpg/, `${newImage}.jpg`);
+                let url = imgElements[i].src.replace(/(hq1|hq2|hq3|hqdefault|mqdefault|hq720)(_custom_[0-9]+)?.jpg/, `${newImage}.jpg`);
 
                 if (!url.match('.*stringtokillcache')) {
                     url += '?stringtokillcache'
@@ -121,9 +121,9 @@ if (typeof window.styleElement === 'undefined') { // shitty way to detect if scr
         for (let i = 0; i < backgroundImgElements.length; i++) {
             let styleAttribute = backgroundImgElements[i].getAttribute('style');
 
-            if (styleAttribute.match('.*https://i.ytimg.com/vi/.*/(hq1|hq2|hq3|hqdefault|mqdefault|hq720|hq720_custom_[0-9]+).jpg?.*')) {
+            if (styleAttribute.match('.*https://i.ytimg.com/(vi|vi_webp)/.*/(hq1|hq2|hq3|hqdefault|mqdefault|hq720)(_custom_[0-9]+)?.jpg?.*')) {
 
-                let newStyleAttribute = styleAttribute.replace(/(hq1|hq2|hq3|hqdefault|mqdefault|hq720|hq720_custom_[0-9]+).jpg/, `${newImage}.jpg`);
+                let newStyleAttribute = styleAttribute.replace(/(hq1|hq2|hq3|hqdefault|mqdefault|hq720)(_custom_[0-9]+)?.jpg/, `${newImage}.jpg`);
 
                 if (!newStyleAttribute.match('.*stringtokillcache.*')) {
                     // messes up existing query parameters that might be there, but that's ok.
